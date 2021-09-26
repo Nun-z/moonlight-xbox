5e7f498d34b652fc0714429f3ee2390e91f9f713
Nun-z <nickwitkin@gmail.com>
2021-09-25 18:47:48 -0700

    fix defects exposed by testing

--- Pages/MenuPage.xaml.cpp
+++ Pages/MenuPage.xaml.cpp
@@ -43,43 +43,48 @@ void moonlight_xbox_dx::MenuPage::ConnectButton_Click(Platform::Object^ sender, 
 	//config = ref new StreamConfiguration();
 	Platform::String^ currentHostname = config->getHostname();
 
-	if (currentHostname == "") {
+	std::string savedHostname = Utils::getTextFromBox(currentHostname);
+	std::string enteredHostname = Utils::getTextFromBox(ipAddressText->Text);
+
+	if ((savedHostname == "") ||
+	((enteredHostname != "") && (savedHostname != enteredHostname))) {
 		currentHostname = this->ipAddressText->Text;
 	}
 	else {
 		this->ipAddressText->Text = currentHostname;
 	}
+	if (this->ipAddressText->Text != "") {
+		config->setHostname(currentHostname);
+		this->progressRing->IsActive = true;
+		client = new MoonlightClient();
+		char ipAddressStr[2048];
+		wcstombs_s(NULL, ipAddressStr, currentHostname->Data(), 2047);
+		int status = client->Connect(ipAddressStr);
+		if (status != 0) {
+			this->connectStatus->Text = L"Cannot Connect";
+			return;
+		}
+		if (!client->IsPaired()) {
+			char* pin = client->GeneratePIN();
+			std::string s_str = std::string(pin);
+			std::wstring wid_str = std::wstring(s_str.begin(), s_str.end());
+			const wchar_t* w_char = wid_str.c_str();
+			Platform::String^ p_string = ref new Platform::String(w_char);
+			this->connectStatus->Text = String::Concat(L"Use PIN for pairing and then press Connect Again: ", p_string);
+			ProgressRing^ ring = progressRing;
+			TextBlock^ text = connectStatus;
+			auto t = Concurrency::create_async([]()
+				{
+					int a = client->Pair();
+				});
+			this->progressRing->IsActive = false;
+		}
+		else {
 
-	config->setHostname(currentHostname);
-	this->progressRing->IsActive = true;
-	client = new MoonlightClient();
-	char ipAddressStr[2048];
-	wcstombs_s(NULL, ipAddressStr, currentHostname->Data(), 2047);
-	int status = client->Connect(ipAddressStr);
-	if (status != 0) {
-		this->connectStatus->Text = L"Cannot Connect";
-		return;
-	}
-	if (!client->IsPaired()) {
-		char* pin = client->GeneratePIN();
-		std::string s_str = std::string(pin);
-		std::wstring wid_str = std::wstring(s_str.begin(), s_str.end());
-		const wchar_t* w_char = wid_str.c_str();
-		Platform::String^ p_string = ref new Platform::String(w_char);
-		this->connectStatus->Text = String::Concat(L"Use PIN for pairing and then press Connect Again: ", p_string);
-		ProgressRing^ ring = progressRing;
-		TextBlock^ text = connectStatus;
-		auto t = Concurrency::create_async([]()
-			{
-				int a = client->Pair();
-			});
-		this->progressRing->IsActive = false;
-	}
-	else {
-
-		this->connectStatus->Text = L"Connected";
-		this->progressRing->IsActive = false;
-		UpdateApps();
+			this->connectStatus->Text = L"Connected";
+			this->progressRing->IsActive = false;
+			UpdateApps();
+		}
 	}
 	//
 }
@@ -104,16 +109,20 @@ void moonlight_xbox_dx::MenuPage::OnAppClicked(Platform::Object^ sender, Windows
 	ComboBoxItem^ item = (ComboBoxItem^)this->appsComboBox->SelectedItem;
 
 	if (Utils::getTextFromBox(WidthTextbox->Text) != "") {
-		config->setWidth(std::stoi(Utils::getTextFromBox(WidthTextbox->Text)));
+		int width = std::stoi(Utils::getTextFromBox(WidthTextbox->Text));
+		concurrency::create_task([width] { config->setWidth(width); } );
 	}
 	if (Utils::getTextFromBox(HeightTextbox->Text) != "") {
-		config->setHeight(std::stoi(Utils::getTextFromBox(HeightTextbox->Text)));
+		int height = std::stoi(Utils::getTextFromBox(HeightTextbox->Text));
+		concurrency::create_task([height] { config->setHeight(height); });
 	}
 	if (Utils::getTextFromBox(FpsTextbox->Text) != "") {
-		config->setFps(std::stoi(Utils::getTextFromBox(FpsTextbox->Text)));
+		int fps = std::stoi(Utils::getTextFromBox(FpsTextbox->Text));
+		concurrency::create_task([fps] { config->setFps(fps); });
 	}
 	if (Utils::getTextFromBox(BitrateTextbox->Text) != "") {
-		config->setBitrate(std::stoi(Utils::getTextFromBox(FpsTextbox->Text)));
+		int bitrate = std::stoi(Utils::getTextFromBox(BitrateTextbox->Text));
+		concurrency::create_task([bitrate] { config->setBitrate(bitrate); });
 	}
 
 	config->setAppID((int)item->DataContext);
@@ -149,7 +158,7 @@ void moonlight_xbox_dx::MenuPage::SaveButton_Click(Platform::Object^ sender, Win
 		concurrency::create_task([bitrate] { config->setBitrate(bitrate); });
 
 		concurrency::create_task([] { config->serializeConfiguration(); });
-		ErrorText->Text = "";
+		ErrorText->Text = "Save was performed successfully.";
 	}
 	catch (std::exception e) {
 		ErrorText->Text = "Save failed.  One or more fields is empty or malformed.";
